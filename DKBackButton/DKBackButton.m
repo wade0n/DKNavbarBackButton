d824f4d490ebaf7bde0a5e07b883bc606736b046
Дмитрий калашников <air.dmitriy@gmail.com>
2013-11-18 12:28:22 +0400

    fix title insets on 5 pixels!

--- DKBackButton/DKBackButton.m
+++ DKBackButton/DKBackButton.m
@@ -27,7 +27,7 @@
 - (void)makeOffset{
     [self setClipsToBounds:NO];
     [self setImageEdgeInsets:UIEdgeInsetsMake(0, - 13, 0, 0)];
-    [self setTitleEdgeInsets:UIEdgeInsetsMake(0, self.imageEdgeInsets.left+10, 0, 0)];
+    [self setTitleEdgeInsets:UIEdgeInsetsMake(0, self.imageEdgeInsets.left+12, 0, 0)];
     [self.titleLabel setFont:[UIFont systemFontOfSize:17.0f]];
 
 }
